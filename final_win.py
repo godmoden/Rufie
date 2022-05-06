98f7440bd68272778655d133fd949678e5ec1b10
godmoden <104829349+godmoden@users.noreply.github.com>
2022-05-06 15:31:33 +0600

    Add files via upload

--- final_win.py
+++ final_win.py
@@ -1,1 +1,25 @@
-
+from instr import *
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.hello_text = QLable(txt_hello)
+        self.instruction = QLable(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.hello_text.addWidget(self.layout)
+        self.instruction.addWidget(self.layout)
+        self.button.addWidget(self.layout)
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
--- second_win.py
+++ second_win.py
@@ -1,1 +1,32 @@
-
+from PyQt5.QtWidgets import *
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        line_name = QLineEdit('Ф.И.О.')
+        name_lable = QLabel('Ведите Ф.И.О.')
+        age_lable = QLabel('Полных лет:')
+        line_age = QLineEdit('0')
+        inster1_lable = Qlable('Лягте на спин и замерьте пульс за 15 секунд. Нажмите кнопку"Начать первый тест", чтобы запустить таймер. Результат запешите в соответствующее поле.') 
+        
+        self.h_line = QVBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.l_line = QVBoxLayout()
+        self.l_line.addWidget(self.btn1)
+        self.r_line.addWidget(self.timer)
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+        self.setLayout(self.h_line)
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.fw.FinalWin()
